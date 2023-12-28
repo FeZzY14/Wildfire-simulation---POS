411427ef4e5d2e9e3938265a15ed2120c076e29a
FeZzY14 <jmisani123gmail.com>
2023-12-28 14:42:34 +0100

    test

---
+++ Bunka.h
@@ -0,0 +1,22 @@
+//
+// Created by jmisa on 28. 12. 2023.
+//
+
+#ifndef SEMPRACA2_BUNKA_H
+#define SEMPRACA2_BUNKA_H
+
+
+class Bunka {
+private:
+    int x;
+    int y;
+
+public:
+    int getX();
+    int getY();
+    int setX();
+    int setY();
+};
+
+
+#endif //SEMPRACA2_BUNKA_H
--- cmake-build-debug/CMakeFiles/SemPraca2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SemPraca2.
-# This may be replaced when dependencies are built.

3546763ff148e0ad317e8d58ffca46c1932b9bba
SamuelTholt <spotke8@gmail.com>
2023-12-28 14:45:38 +0100

    pridanie Enumu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "PoziarnyBiotop.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+             char poziar = static_cast<char>(PoziarBiotop::Voda);
+             std::cout << poziar << " ";
+        }
+        std::cout << "\n";
+
+    }
     return 0;
 }

f93501a776c050ca88a70321a30241f3e8cbe95a
SamuelTholt <spotke8@gmail.com>
2023-12-28 15:53:10 +0100

    Zmena_v_maine

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10; ++j) {
-             Bunka bunka = Bunka(0, 0, PoziarBiotop::Poziar);
+             Bunka bunka = Bunka(0, 0, PoziarBiotop::Voda);
              std::cout << bunka.getZnak() << " ";
         }
         std::cout << "\n";

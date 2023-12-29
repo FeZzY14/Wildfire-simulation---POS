21274f25494673812ddd5be12b517ef92deabfb6
SamuelTholt <spotke8@gmail.com>
2023-12-29 14:28:37 +0100

    Generacia Svetu oprava

--- main.cpp
+++ main.cpp
@@ -4,18 +4,12 @@
 #include "Bunka.h"
 #include "Svet.h"
 
-struct nieco {
-    int data = 0;
-};
 
 int main() {
     Svet svet = Svet(20, 5);
     svet.vytvorSvet();
     svet.vytvorPoziarRandomPosition();
-    //svet.spusti();
-
-    nieco data;
-    std::thread(svet.spusti, &data);
-
+    std::thread svetThread(&Svet::spusti, &svet);
+    svetThread.join();
     return 0;
 }
